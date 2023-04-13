c50f4110853768b7683658f677779347f80b6751
Dinesh Chakravarthi Kola <k.dineshchakravarthi@gmail.com>
2023-04-13 15:03:17 +0530

    added new methods

--- os lab project.py
+++ os lab project.py
@@ -18,4 +18,38 @@ class Dircmp:
         
         return allFiles, allDirs
     
+    def onlys(self, list1, list2):
+        
+        only_in_list1 = []
+        for item in list1:
+            if item not in list2:
+                only_in_list1.append(item)
+
+        return only_in_list1
+    
+    
+
+    def commons(self, list1, list2):
+       
+
+        comm_items = []
+        for item in list1[:]:
+            if item in list2[:]:
+                comm_items.append(item)
+
+        return comm_items
+    
+    
+
+    def common_but(self):
+        
+
+        files = []
+        join = os.path.join
+        for file in self.comm_files[:]:
+            if not filecmp.cmp(join(self.d1, file), join(self.d2, file), shallow = False):
+                files.append( ( join ( self.d1, file ), join ( self.d2, file ) ) )
+        self.funnies = files[:]
+        return files
+    
     
